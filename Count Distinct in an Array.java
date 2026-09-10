1f8b27b85fbde78000078e298e1f4b4949067f50
Abhay Saini <siddsaini0906@gmail.com>
2026-09-10 22:22:50 +0530

    Update Count Distinct in an Array.java

--- Count Distinct in an Array.java
+++ Count Distinct in an Array.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    static int countDistinct(int arr[]) {
+        HashSet <Integer> set = new HashSet<>();
+        for(int i : arr){
+            set.add(i);
+        }
+        return set.size();
+        
+    }
+}
