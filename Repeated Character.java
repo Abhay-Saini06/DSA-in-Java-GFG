6fbe20ffde0de7160e45161655ad689875cedd73
Abhay Saini <siddsaini0906@gmail.com>
2026-09-11 14:58:18 +0530

    Create Repeated Character.java

---
+++ Repeated Character.java
@@ -0,0 +1,18 @@
+class Solution {
+    char firstRep(String S) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : S.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : S.toCharArray()) {
+            if(map.get(c) > 1) {
+                return c;
+            }
+        }
+
+        return '#';
+    }
+}
