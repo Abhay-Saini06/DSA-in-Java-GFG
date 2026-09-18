212642ff2d09e7df757506cd827c002ecd79f57e
Abhay Saini <siddsaini0906@gmail.com>
2026-09-18 14:42:28 +0530

    Create At Least K Occurrences.java

---
+++ At Least K Occurrences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstElementKTime(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+
+            if (map.get(i) == k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
