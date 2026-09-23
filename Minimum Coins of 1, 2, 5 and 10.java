327b0e511c136d4e797b4d7ef60230b842bad514
Abhay Saini <siddsaini0906@gmail.com>
2026-09-23 17:45:17 +0530

    Create Minimum Coins of 1, 2, 5 and 10.java

---
+++ Minimum Coins of 1, 2, 5 and 10.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMin(int n) {
+        // code here
+        int []coins = {1,2,5,10};
+        int count = 0;
+        for(int i = coins.length-1;i>=0;i--){
+            while(n>=coins[i]){
+                n = n-coins[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
